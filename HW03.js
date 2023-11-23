fa0f982a21caaedd2efafd239426c3496bad5d5c
72Maksim03 <149401460+72Maksim03@users.noreply.github.com>
2023-11-24 01:33:35 +0400

    Create HW03.js

---
+++ HW03.js
@@ -0,0 +1,53 @@
+// 1. Insert a number. Print “yes” if it contains 3 successive zeros, otherwise print “no”.
+
+function detectZero(number){
+  for(number; number > 10; number = Math.round(number / 10)){
+    if(number % 1000 === 0){
+      console.log("yes")
+      return
+    }
+  }
+  console.log("no")
+}
+detectZero(100011110)
+
+// 2. Insert a number. Remove all zeros from the number, except the last one and print the number. If there are at most one zero, print “Nothing to remove”.
+
+function removeZeroes(number){
+  let i = String(number)
+  let index = i.length
+  let result = ""
+  let f = 0
+  for(let j = index-1; j > -1; j--){
+    if(i[j] === '0'){
+      f++
+    }
+    if(f < 2){
+      result = i[j] + result
+    }else if(i[j] !== '0'){
+      result = i[j] + result
+    }
+  }
+  if (result === i){
+    console.log("Nothig to remove")
+  }else{
+    console.log(result)
+  }
+}
+removeZeroes(4050120)
+
+// 3. Insert a n positive number. Check number is prime or not. Prime numbers are numbers that have only 2 factors: 1 and themselves. For example, the first 5 prime numbers are 2, 3, 5, 7, and 11.
+
+function prime(number){
+  for(let i = 2; i < number; i++){
+    if(number % i === 0){
+      console.log(false)
+      return
+    }
+  }
+  console.log(true)
+}
+prime(23)
+
+// 4. Insert a n positive number. Print the n-st prime number.
+
